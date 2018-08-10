d62e89b1e7d28b23b04cd92d012868d1b4d89217
surya <suryaherdiyanto@gmail.com>
2018-08-10 19:36:59 +0800

    migration

---
+++ database/migrations/2018_08_10_112256_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nim', 50);
+            $table->string('name', 100);
+            $table->date('dob');
+            $table->string('phone', 15);
+            $table->text('address');
+            $table->boolean('genre');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
